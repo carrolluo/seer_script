19cf9c014ae84e7f87edd21fec0afd1bdf09ac22
luo_xuan <luo_xuan@163.net>
2025-06-08 22:31:31 +0800

    v609

--- script6.py
+++ script6.py
@@ -47,16 +47,49 @@ def do_catch():
         catch_count += 1
 
 
+def catch_if(monster, count):
+    if find_image("./special_monster/nier.png", confidence=0.95): 
+        time.sleep(2)
+        monster_in_battle = r'./monster_in_battle/nier'
+        monster_in_battle_file = get_all_file_paths(monster_in_battle)
+        if find_image(monster_in_battle_file,confidence=0.999,timeout=3):
+            do_run()
+        else:
+            count += 299900000
+            do_catch()
+           
+    elif find_image("./special_monster/zhake.png" ,confidence= 0.95):
+        time.sleep(2)
+        monster_in_battle = r'./monster_in_battle/zhake' 
+        monster_in_battle_file = get_all_file_paths(monster_in_battle)
+        if find_image(monster_in_battle_file,confidence=0.999,timeout=3):
+            do_run()
+        else:
+            count += 199900000
+            do_catch()
+
+    else:
+        monster_in_battle = r'./monster_in_battle/' + monster
+        monster_in_battle_file = get_all_file_paths(monster_in_battle)
+        if find_image(monster_in_battle_file,confidence=0.999,timeout=3):
+            do_run()
+        else:
+            count += 99900000
+            if find_image('./ui/prop.png', confidence=0.95):
+                do_catch()
+            else:
+                do_fight()      
+    return count 
 
 def print_func(count):
     print(f"round : {count}")
 
 def main():
-    count = 13661
+    count = 0
 
     heal_round = 10
 
-    monster_name_py = "youfu"
+    monster_name_py = "jier"
     monster = get_all_file_paths('./learning_ability/'+ monster_name_py)
 
     while True:
