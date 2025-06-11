30bd900d849633eecd24b094b2ed4c3dbb0904b6
luo_xuan <luo_xuan@163.net>
2025-06-11 20:31:45 +0800

    v611

--- script6.py
+++ script6.py
@@ -1,22 +1,13 @@
 from common import *
 
 def do_catch():
-    time.sleep(1)
-    find_and_click("./skills/bokeer/shouxialiuqing.png",confidence=0.8)
+    time.sleep(1.5)
+    move_and_click_func(skill4)
     pyautogui.moveTo(center_location[0], center_location[1], duration=0.1)
     time.sleep(1)
     pyautogui.mouseDown()
     time.sleep(0.05)  # 停留一点点
     pyautogui.mouseUp()
-
-    while not find_and_click("./switch_icon/lisha.png",confidence=0.8):
-        while (not find_and_click("./ui/switch.png", confidence=0.8)):
-            pyautogui.moveTo(center_location[0], center_location[1], duration=0.1)
-            find_and_click("./ui/fight.png", confidence=0.8)
-            time.sleep(0.2)
-        time.sleep(0.2)
-    find_and_click("./ui/go_out.png", confidence=0.9)
-    time.sleep(3)
 
     catch_count = 1
 
@@ -52,20 +43,23 @@ def catch_if(monster, count):
         time.sleep(2)
         monster_in_battle = r'./monster_in_battle/nier'
         monster_in_battle_file = get_all_file_paths(monster_in_battle)
-        if find_image(monster_in_battle_file,confidence=0.999,timeout=3):
-            do_run()
-        else:
+        if not find_image(monster_in_battle_file,confidence=0.999,timeout=3):
             count += 299900000
             do_catch()
+        else:
+            if find_image('./ui/nier46.png',confidence=0.99,timeout=3):
+                do_catch()
+            else:
+                do_run()
            
     elif find_image("./special_monster/zhake.png" ,confidence= 0.95):
         time.sleep(2)
         monster_in_battle = r'./monster_in_battle/zhake' 
         monster_in_battle_file = get_all_file_paths(monster_in_battle)
-        if find_image(monster_in_battle_file,confidence=0.999,timeout=3):
-            do_run()
-        else:
+        if not find_image(monster_in_battle_file,confidence=0.999,timeout=3):
             count += 199900000
+            do_catch()
+        else:
             do_catch()
 
     else:
@@ -74,7 +68,7 @@ def catch_if(monster, count):
         if find_image(monster_in_battle_file,confidence=0.999,timeout=3):
             do_run()
         else:
-            count += 99900000
+            count += 99900000000
             if find_image('./ui/prop.png', confidence=0.95):
                 do_catch()
             else:
@@ -85,7 +79,7 @@ def print_func(count):
     print(f"round : {count}")
 
 def main():
-    count = 0
+    count = 13039
 
     heal_round = 10
 
